921f04efb04f963a9c540101116e3008b63a686c
AbhishekPandey12 <abhisonu3629@gmail.com>
2020-05-17 19:52:01 +0530

    Observable Creation through from, of

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit(){
+
+    of(2, 3, 4, 6, 8).subscribe(console.log);
+
+    from([20, 40, 60, 80]).subscribe(
+      item => console.log(`Item is.... ${item}`),
+      err => console.log(`Got Error`),
+      () => console.log(`Complete`)
+    )
+  }
 }
